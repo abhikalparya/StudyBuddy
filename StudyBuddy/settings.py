c4be6b8ad9782a806a7d849464d7a5e0e1cc9142
Abhikalp Arya <abhikalp.arya@gmail.com>
2023-02-02 14:49:52 +0530

    updated settings file

--- StudyBuddy/settings.py
+++ StudyBuddy/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_ROOT = BASE_DIR / 'static'
